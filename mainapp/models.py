033a713ca7116c10a6fa6f748d7759bd25a40c15
ahsan781 <83701883+code4ualways@users.noreply.github.com>
2022-08-31 17:02:50 +0500

    new changes is added

---
+++ mainapp/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+# Create your models here.
+class CommunityTable(models.Model):
+    
+    Email = models.CharField(max_length=100)
+    Age = models.CharField(max_length=100)
+    Gender_choices = (
+        ("Male", "Male"),
+        ("Female", "Female"),
+        ("other", "other"),
+       
+    )
+
+    Gender = models.CharField(max_length=50, choices=Gender_choices)
+    Qualification_choices = Gender_choices = (
+        ("Master's Degree", "Master's Degree"),
+        ("Bachelor's Degree", "Bachelor's Degree"),
+        ("Higher Secondary School", "Higher Secondary School"),
+        ("Secondary School", "Secondary School"),
+        ("No Formal Schooling", "No Formal Schooling"),
+        ("Other", "Other"),
+       
+    )
+    qualification = models.CharField(max_length=100, choices=Qualification_choices)
+    income_choices = (
+        ("Low income", "Low income"),
+        ("Middle income", "Middle income"),
+        ("High income", "High income"),
+       
+    )
+
+    income = models.CharField(max_length=50, choices=income_choices)
+    Occupation = models.CharField(max_length=100)
+    comments = models.CharField(max_length=100)
+    Name_suggestion = models.CharField(max_length=100)
+    Other = models.JSONField()
+    def __str__(self):
+        return self.id
+
+    
+    
+    
