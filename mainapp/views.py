f1ad6b330e4950aa9660e42667144c1fba5ff62a
ahsan781 <83701883+code4ualways@users.noreply.github.com>
2022-09-02 18:07:02 +0500

    new chnages is added

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,2 +1,11 @@
+from genericpath import commonprefix
+from django.shortcuts import render
+from .models import *
+from .serializer import *
+from rest_framework.viewsets import ModelViewSet
+# Create your views here.
 
-
+class CommunityViewDashboard(ModelViewSet):
+    queryset = CommunityTable.objects.all()
+    serializer_class = CommunitySerlizer
+ 
